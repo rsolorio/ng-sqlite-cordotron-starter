9dab53efc45bbb212802f2009ee5d7321f71ef16
Roberto Solorio <robertosolorio@hotmail.com>
2022-12-23 22:05:01 +0000

    Initial commit. Browser and electron support

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# ng-sqlite-cordotron-starter
+# Angular Sqlite (TypeOrm) Cordova Electron
+
+Boilerplate project that supports an Angular/Sqlite application running in Electron or Cordova.
+
+## Technologies & Tools
+- Angular CLI 14
+- NodeJS 18
+- Android Studio Dolphin 2021.3.1 Patch 1
+  - Which provides the Android SDK 9.0 (Pie)
+- Cordova 11
+
+## NPM package scripts
+- npm run serve:browser
+  - Serves and runs the application in a browser window
+- npm run serve:electron
+  - Serves and runs the application in electron, supporting auto refresh when files change
+- npm run dist:electron
+  - Runs the application in electron, retrieving the content from the dist folder
+- npm run build:windows
+  - Builds the application for electron supporting Windows. Output folder: prod
+
+## Global Dependencies
+- @angular/cli@14
+- cordova@11
+
+## Dev Dependencies
+- electron-reload
+  - Used in the main electron script to reload electron and react to changes.
+- wait-on
+  - Used in the script that serves electron from localhost; it waits for the default url to be loaded before preparing the main electron script.
+- npm-run-all
+  - To run scripts in parallel. In this case used to run the ng serve and the electron preparation at the same time.
+- electron-builder
+  - Used to generate the build for windows or mac.
---
+++ main-electron.ts
@@ -0,0 +1,93 @@
+import { app, BrowserWindow, ipcMain, dialog } from 'electron';
+import * as remoteMain from '@electron/remote/main';
+import * as url from 'url';
+import * as path from 'path';
+
+let mainWindow: BrowserWindow;
+const appName = 'ng-sqlite-cordotron-starter';
+
+// Detect serve mode (Development mode)
+const args = process.argv.slice(1);
+const serve: boolean = args.some(val => val === '--serve');
+remoteMain.initialize();
+
+function createWindow(): void {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: false
+    },
+    icon: path.join(__dirname, `/dist/${appName}/favicon.ico`),
+  });
+  mainWindow.maximize();
+  remoteMain.enable(mainWindow.webContents);
+
+  if (serve) {
+    console.log('Loading electron from localhost with reload capability.');
+    require('electron-reload')(__dirname, {
+        electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+    });
+    mainWindow.loadURL('http://localhost:4200');
+    mainWindow.show();
+    mainWindow.webContents.openDevTools();
+  } else {
+    console.log('Loading electron from dist folder without reload capability.');
+    const sitePath = path.join(__dirname, `/dist/${appName}/index.html`);
+    mainWindow.loadURL(
+      url.format({
+        pathname: sitePath,
+        protocol: 'file',
+        slashes: true,
+      }),
+    );
+  }
+
+  //mainWindow.setMenuBarVisibility(false);
+  mainWindow.setIcon(path.join(__dirname, `/dist/${appName}/favicon.ico`));
+
+  mainWindow.on('closed', () => {
+    mainWindow.destroy();
+  });
+
+}
+
+try {
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+      }
+
+      ipcMain.handle('openFolderDialog', (e, options) => {
+        options = options ? options : {};
+        options.properties = ['openDirectory'];
+        return dialog.showOpenDialogSync(options);
+      });
+
+      ipcMain.handle('openDevTools', () => {
+        mainWindow.webContents.openDevTools();
+      });
+    });
+
+    // Quit when all windows are closed, except on macOS. There, it's common
+    // for applications and their menu bar to stay active until the user quits
+    // explicitly with Cmd + Q.
+    app.on('window-all-closed', () => {
+      if (process.platform !== 'darwin') {
+        app.quit();
+      }
+    });
+  });
+} catch (e) {
+  console.log(e);
+}
